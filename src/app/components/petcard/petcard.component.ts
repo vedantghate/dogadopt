27ce9d0b7c73cbf0a6af058be8ea2cac2ff11630
vedantghate <wantedgutsy@gmail.com>
2022-08-30 11:25:05 -0400

    Added customization to pet card

--- src/app/components/petcard/petcard.component.ts
+++ src/app/components/petcard/petcard.component.ts
@@ -10,10 +10,16 @@ export class PetcardComponent implements OnInit {
   @Input() name: string;
   @Input() imgSrc: string;
   @Input() description: string;
+  @Input() small: boolean;
+
+  smallDesc: string;
+  smallCard: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.smallCard = this.small ? '23rem' : '30rem';
+    this.smallDesc = this.small ? '3rem' : '9rem';
   }
 
 }
